a89f39bafafa17703c6eefff7c15f0b096e1e14f
Mohit <mohitd2288@gmail.com>
2023-12-13 17:28:02 +0530

    :lock: Protect dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,8 @@ import { User } from '@firebase/auth'
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { auth } from '@/firebase/firebase';
+import WishList from '@/components/WishList';
+import PostForm from '@/components/PostForm';
 
 function Dashboard(): React.ReactNode {
     const [authUser, setAuthUser] = useState<User | null>(null);
@@ -19,7 +21,10 @@ function Dashboard(): React.ReactNode {
             unsubscribe();
         };
     }, []);
-    return (authUser && <div>Dashboard</div>);
+    return (authUser == null ? <h1>Loading</h1> : <div className="flex flex-col-reverse lg:flex-row items-center gap-8 justify-between">
+        <WishList />
+        <PostForm />
+    </div>);
 }
 
 export default Dashboard;
