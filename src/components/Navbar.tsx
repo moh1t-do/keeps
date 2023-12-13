884b41daca1927a6530f942bab26edb0b53e9d14
Mohit <mohitd2288@gmail.com>
2023-12-13 19:12:29 +0530

    :lipstick: Update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,8 +41,8 @@ function Navbar(): React.ReactNode {
 
     return (
         <div className="flex h-20 w-full items-center justify-between uppercase border-b">
-            <Link href="/" className="cursor-pointer">
-                Link
+            <Link href="/" className="cursor-pointer outline-none">
+                WISHES
             </Link>
             <div>
                 <button className="sm:hidden" onClick={(): void => setIsOpen(!isOpen)}>
@@ -57,21 +57,20 @@ function Navbar(): React.ReactNode {
                                 </Link>
                             </li>
                         ))}
-                        {authUser && <li><button onClick={handleSignOut} className="align-middle h-8 px-4 py-2 select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs bg-blue-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Logout</button></li>}
+                        {authUser && <li><button onClick={handleSignOut} className="h-8 px-4 py-2 font-bold  uppercase transition-all  text-xs bg-blue-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Logout</button></li>}
                     </ul>
                 </div>
                 {/* mobile  */}
                 {isOpen && (
-                    <div className="w-screen fixed sm:hidden top left-0 h-full bg-black">
+                    <div className="w-screen fixed sm:hidden top left-0 h-full py-4 bg-black">
                         <div className="max-w-6xl mx-auto px-8">
-                            <ul className="flex flex-col gap-10 cursor-pointer py-8">
+                            <ul className="flex flex-col gap-10 cursor-pointer py-4">
                                 {navItems.map((ele: navItem, idx: number) => (
-                                    <div key={idx} className="border-b">
-                                        <Link className="mx-auto py-4" href={ele.navLink}>
-                                            {ele.title}
-                                        </Link>
-                                    </div>
+                                    <Link key={idx} className="mx-auto py-2" href={ele.navLink}>
+                                        {ele.title}
+                                    </Link>
                                 ))}
+                                {authUser && <li><button onClick={handleSignOut} className="h-8 px-4 py-2 font-bold  uppercase transition-all  text-xs bg-blue-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Logout</button></li>}
                             </ul>
                         </div>
                     </div>
